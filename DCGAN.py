bade83046a45bf38aa037af573d5b24d5d34b6b5
veetsin <ntqinwei@foxmail.com>
2018-03-12 01:51:54 +0300

    add custom metric

--- DCGAN.py
+++ DCGAN.py
@@ -11,8 +11,8 @@ from mxnet.gluon import nn , utils
 from mxnet import autograd
 import numpy as np
 
-epochs = 2
-batch_size = 128
+epochs = 201
+batch_size = 64
 latent_z_size = 100
 
 def try_gpu():
@@ -38,6 +38,7 @@ for _,_,files in os.walk(data_path):
         img_arr = mx.image.imread(img_dir)
         #from HWC transpose to format of CHW 
         img_arr = nd.transpose(img_arr,(2,0,1))
+        #normalize to [-1,1]
         img_arr = (img_arr.astype(np.float32)/127.5-1)
         img_arr = nd.array(img_arr.reshape((1,)+img_arr.shape))
         img_list.append(img_arr)
@@ -47,10 +48,11 @@ train_data = mx.io.NDArrayIter(data = nd.concatenate(img_list),batch_size=batch_
 def visualize(img_arr):
     #from CHW transpose to format HWC
     plt.imshow(((img_arr.asnumpy().transpose(1,2,0)+1.0)*127.5).astype(np.uint8)) 
+    plt.axis('off')
  
 #show 9 images
 fig = plt.figure(figsize=(9,9))
-for i in range(21):
+for i in range(9):
     plt.subplot(3,3,i+1)
     visualize(img_list[i][0])
 plt.show()
@@ -99,15 +101,22 @@ netG.initialize(mx.init.Normal(.02),ctx=ctx)
 netD.initialize(mx.init.Normal(.02),ctx=ctx)
 
 trainerG = gluon.Trainer(netG.collect_params(),'adam',{'learning_rate':lr,'beta1':beta1})
-trainerD = gluon.Trainer(netG.collect_params(),'adam',{'learning_rate':lr,'beta1':beta1})
+trainerD = gluon.Trainer(netD.collect_params(),'adam',{'learning_rate':lr,'beta1':beta1})
 
 #training loop
-from datetime import datetime
 import time
 import logging 
 
 real_label = nd.ones((batch_size,),ctx=ctx)
 fake_label = nd.zeros((batch_size,),ctx=ctx)
+
+def eveluate(pred , label):
+    pred = pred.flatten()
+    label = label.flatten()
+    return ((pred>.5) == label).mean()
+metric = mx.metric.CustomMetric(eveluate)
+
+logging.basicConfig(level=logging.DEBUG)
 
 for epoch in range(epochs):
     start_time = time.time()
@@ -120,12 +129,14 @@ for epoch in range(epochs):
         with autograd.record():
             output = netD(data).reshape((-1,1))
             errD_real = loss(output,real_label)
+            metric.update([output,],[real_label,])
             
             fake = netG(latent_z)
             output_fake = netD(fake).reshape((-1,1))
             errD_fake = loss(output_fake,fake_label)
             errD = errD_real + errD_fake
             errD.backward()
+            metric.update([output_fake],[fake_label])
         
         trainerD.step(batch_size)
             
@@ -133,20 +144,25 @@ for epoch in range(epochs):
         with autograd.record():
             fake = netG(latent_z)
             output_fake = netD(fake).reshape((-1,1))
-            errG = loss(output_fake,fake_label)
+            errG = loss(output_fake,real_label)
             errG.backward()
         
         trainerG.step(batch_size)
-    fake = netG(latent_z)
-    fig = plt.figure(figsize=(16,16))
-    if (epoch%10) ==0
-    	for i in range(4):
-        	plt.subplot(2,2,i+1)
-        	visualize(fake[i])
-    	plt.show()
-    	end_time = time.time() 
-    print('time of 10 epochs:%f'%(end_time-start_time))
-        
+      
+    end_time = time.time() 
+    _,acc = metric.get()
+    logging.info('epoch: %i ; discriminator loss:%f ; generator loss:%f ; training acc:%f ; time:%f'
+                 %(epoch , nd.mean(errD).asscalar(),nd.mean(errG).asscalar(),acc,end_time-start_time))
+    metric.reset()
+    if (0 < epoch < 10) or ((epoch % 10) == 0):
+        fig = plt.figure(figsize=(12,12))
+        for i in range(4):
+            latent_z = mx.nd.random_normal(0,1,shape=(4,latent_z_size,1,1),ctx=ctx)
+            fake = netG(latent_z)
+            plt.subplot(2,2,i+1)
+            visualize(fake[i])
+        plt.show()
+    
 
         
     
